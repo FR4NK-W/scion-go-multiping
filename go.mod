0d92d5e041e261d4826421b469d612f8f0aaaf0a
FR4NK-W <wirzf@inf.ethz.ch>
2025-01-13 22:22:51 +0100

    Multiping Concurrent ping to multiple destinations

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/netsec-ethz/scion-go-multiping
+
+go 1.22.7
+
+require github.com/scionproto/scion v0.11.0
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/google/gopacket v1.1.19 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/prometheus/client_golang v1.19.1 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.53.0 // indirect
+	github.com/prometheus/procfs v0.14.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,331 @@
+package main
+
+import (
+	"context"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"net/netip"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/scionproto/scion/pkg/addr"
+	"github.com/scionproto/scion/pkg/log"
+	"github.com/scionproto/scion/pkg/private/common"
+	"github.com/scionproto/scion/pkg/private/serrors"
+	"github.com/scionproto/scion/private/topology/underlay"
+	"github.com/scionproto/scion/pkg/snet"
+	"github.com/scionproto/scion/pkg/snet/path"
+	"github.com/scionproto/scion/pkg/sock/reliable"
+)
+
+func main() {
+	fmt.Println("Go multiping")
+
+	saddr := net.UDPAddr{IP: getSaddr(), Port: 0}
+	sia := addr.MustIAFrom( addr.ISD(64), addr.AS(8589934601))
+	dia := addr.MustIAFrom( addr.ISD(71), addr.AS(559))
+	dhost := net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 0}
+	remote := snet.UDPAddr{IA: dia, Host: &dhost}
+	destIAs := []snet.UDPAddr{remote, snet.UDPAddr{IA: addr.MustIAFrom( addr.ISD(71), addr.AS(8589934666)), Host: &dhost}}
+
+	for _, r := range destIAs {
+		// Short interval
+		go runPing(sia, saddr, r, 10*time.Second)
+	}
+
+	// Long interval
+	runPing(sia, saddr, remote, 60*time.Second)
+
+	// Path prober
+	//
+}
+
+func runPing(sia addr.IA, saddr net.UDPAddr, r snet.UDPAddr, interval time.Duration) {
+	ctx := context.TODO()
+	replies := make(chan reply, 50)
+	id := snet.RandomSCMPIdentifer()
+	svc := snet.DefaultPacketDispatcherService{
+		Dispatcher: reliable.NewDispatcher(""),
+		SCMPHandler: scmpHandler{
+			id:      id,
+			replies: replies,
+		},
+	}
+
+	conn, port, err := svc.Register(ctx, sia, &saddr, addr.SvcNone)
+	if err != nil {
+		return
+	}
+	saddr.Port = int(port)
+
+	p := pinger{
+		interval: 10 * time.Second,
+		timeout:  time.Second,
+
+		id:            id,
+		conn:          conn,
+		local:         &snet.UDPAddr{IA: sia, Host: &saddr},
+		replies:       replies,
+		errHandler:    nil,
+		updateHandler: nil,
+	}
+	p.Ping(ctx, &r)
+}
+
+func getSaddr() net.IP {
+	ip := net.ParseIP("129.132.19.216")
+	udpAddr := net.UDPAddr{IP: ip, Port: 443}
+	if conn, err := net.DialUDP(udpAddr.Network(), nil, &udpAddr); err == nil {
+		return net.ParseIP(conn.LocalAddr().String())
+	}
+	return nil
+}
+
+type Update struct {
+	Size int
+	Source snet.SCIONAddress
+	Sequence int
+	RTT time.Duration
+	State State
+}
+
+type State int
+
+const (
+	Success State = iota
+	AfterTimeout
+	Duplicate
+)
+
+type Stats struct {
+	Sent int
+	Received int
+	AvgLatency float64
+}
+
+type pinger struct {
+	interval time.Duration
+	timeout time.Duration
+
+	id uint16
+	conn snet.PacketConn
+	local *snet.UDPAddr
+	replies <- chan reply
+	errHandler func(error)
+	updateHandler func(Update)
+
+	pld []byte
+	sentSequence int
+	receivedSequence int
+	stats Stats
+}
+
+func (p *pinger) Ping(ctx context.Context, remote *snet.UDPAddr) (Stats, error) {
+	p.sentSequence, p.receivedSequence = -1, -1
+	send := time.NewTicker(p.interval)
+	defer send.Stop()
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	errSend := make(chan error, 1)
+
+	go func() {
+		defer func() {
+			log.Debug("Draining")
+		}()
+		p.drain(ctx)
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		// sender
+
+		for {
+			if err := p.send(remote); err != nil {
+				errSend <- serrors.WrapStr("sending", err)
+				continue
+			}
+			select {
+			case <-send.C:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return p.stats, nil
+		case err := <-errSend:
+			return p.stats, err
+		case reply := <-p.replies:
+			if reply.Error != nil {
+				if p.errHandler != nil {
+					p.errHandler(reply.Error)
+				}
+				continue
+			}
+			p.receive(reply)
+		}
+	}
+}
+
+func (p *pinger) send(remote *snet.UDPAddr) error {
+	sequence := p.sentSequence + 1
+
+	binary.BigEndian.PutUint64(p.pld, uint64(time.Now().UnixNano()))
+	pkt, err := packSCMPrequest(p.local, remote, snet.SCMPEchoRequest{
+		Identifier: p.id,
+		SeqNumber: uint16(sequence),
+		Payload: p.pld,
+	})
+	if err != nil {
+		return err
+	}
+	nextHop := remote.NextHop
+	if nextHop == nil && p.local.IA.Equal(remote.IA) {
+		nextHop = &net.UDPAddr{
+			IP: remote.Host.IP,
+			Port: underlay.EndhostPort,
+			Zone: remote.Host.Zone,
+		}
+	}
+	if err := p.conn.WriteTo(pkt, nextHop); err != nil {
+		return err
+	}
+
+	p.sentSequence = sequence
+	p.stats.Sent++
+	return nil
+}
+
+func (p *pinger) receive(reply reply) {
+	rtt := reply.Received.Sub(time.Unix(0, int64(binary.BigEndian.Uint64(reply.Reply.Payload))))
+	var state State
+	switch {
+	case rtt > p.timeout:
+		state = AfterTimeout
+	case int(reply.Reply.SeqNumber) == p.receivedSequence:
+		state = Duplicate
+	case int(reply.Reply.SeqNumber) == p.receivedSequence + 1:
+		state = Success
+		p.receivedSequence = int(reply.Reply.SeqNumber)
+	default:
+	}
+
+	p.stats.Received++
+	if p.updateHandler != nil {
+		p.updateHandler(Update{
+			RTT: rtt,
+			Sequence: int(reply.Reply.SeqNumber),
+			Size: reply.Size,
+			Source: reply.Source,
+			State: state,
+		})
+	}
+}
+
+func (p *pinger) drain(ctx context.Context) {
+	var last time.Time
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			var pkt snet.Packet
+			var ov net.UDPAddr
+			if err := p.conn.ReadFrom(&pkt, &ov); err != nil && p.errHandler != nil {
+				if now := time.Now(); now.Sub(last) > time.Second {
+					p.errHandler(serrors.WrapStr("straggler packet", err))
+					last = now
+				}
+			}
+		}
+	}
+}
+
+func packSCMPrequest(local, remote *snet.UDPAddr, req snet.SCMPEchoRequest) (*snet.Packet, error) {
+	_, isEmpty := remote.Path.(path.Empty)
+	if isEmpty && !local.IA.Equal(remote.IA) {
+		return nil, serrors.New("no path to remote IA", "local", local.IA, "remote", remote.IA)
+	}
+	remoteHostIP, ok := netip.AddrFromSlice(remote.Host.IP)
+	if !ok {
+		return nil, serrors.New("invalid remote IP", "ip", remote.Host.IP)
+	}
+	localHostIP, ok := netip.AddrFromSlice(local.Host.IP)
+	if !ok {
+		return nil, serrors.New("invalid local IP", "ip", local.Host.IP)
+	}
+	pkt := &snet.Packet{
+		PacketInfo: snet.PacketInfo{
+			Destination: snet.SCIONAddress{
+				IA: remote.IA,
+				Host: addr.HostIP(remoteHostIP),
+			},
+			Source: snet.SCIONAddress{
+				IA: local.IA,
+				Host: addr.HostIP(localHostIP),
+			},
+			Path: remote.Path,
+			Payload: req,
+		},
+	}
+	return pkt, nil
+}
+
+type reply struct {
+	Received time.Time
+	Source   snet.SCIONAddress
+	Size int
+	Reply snet.SCMPEchoReply
+	Error error
+}
+
+type scmpHandler struct {
+	id	uint16
+	replies chan<- reply
+}
+
+func (h scmpHandler) Handle(pkt *snet.Packet) error {
+	echo, err := h.handle(pkt)
+
+	h.replies <- reply{
+		Received: time.Now(),
+		Source: pkt.Source,
+		Size: len(pkt.Bytes),
+		Reply: echo,
+		Error: err,
+	}
+	return nil
+}
+
+func (h scmpHandler) handle(pkt *snet.Packet) (snet.SCMPEchoReply, error) {
+	if pkt.Payload == nil {
+		return snet.SCMPEchoReply{}, serrors.New("no timing payload found")
+	}
+	switch s := pkt.Payload.(type) {
+	case snet.SCMPEchoReply:
+		r := pkt.Payload.(snet.SCMPEchoReply)
+		if r.Identifier != h.id {
+			return snet.SCMPEchoReply{}, serrors.New("wrong SCMP ID",
+				"expected", h.id, "actual", r.Identifier)
+		}
+		return r, nil
+	case snet.SCMPExternalInterfaceDown:
+		return snet.SCMPEchoReply{}, serrors.New("external interface down",
+			"isd_as", s.IA, "interface", s.Interface)
+	default:
+	}
+	return snet.SCMPEchoReply{}, serrors.New("not an SCMPEchoReply",
+		"type", common.TypeOf(pkt.Payload))
+}
