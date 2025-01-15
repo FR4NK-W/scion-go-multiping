cc7d3512f1373b145344f3e97ff1579e8fd4ef45
FR4NK-W <wirzf@inf.ethz.ch>
2025-01-15 10:16:11 +0100

    Add comments to gorm fields

--- exporter.go
+++ exporter.go
@@ -8,28 +8,28 @@ import (
 
 type PingResult struct {
 	gorm.Model
-	SrcSCIONAddr    string
-	DstSCIONAddr    string
-	Success         bool
-	Latency         float64
-	PingTime        time.Time
-	SuccessfulPings int
-	MaxPings        int
+	SrcSCIONAddr    string // SCION src
+	DstSCIONAddr    string // SCION dst
+	Success         bool // SuccessfulPings > 0
+	Latency         float64 // min latency across path probed
+	PingTime        time.Time // time ping result was stored
+	SuccessfulPings int // Ping replies count
+	MaxPings        int // Sent ping count
 }
 
 type PathStatistics struct {
 	gorm.Model
-	SrcSCIONAddr   string
-	DstSCIONAddr   string
-	Success        bool
-	MinLatency     float64
-	MaxLatency     float64
-	MinHops        int
-	MaxHops        int
-	LookupTime     time.Time
-	ActivePaths    int
-	ProbedPaths    int
-	AvailablePaths int
+	SrcSCIONAddr   string // SCION src
+	DstSCIONAddr   string // SCION dst
+	Success        bool // successCount > 0
+	MinLatency     float64 // min latency across all paths
+	MaxLatency     float64 // min latency across all paths
+	MinHops        int // min # of hops across all paths
+	MaxHops        int // max # of hops across all paths
+	LookupTime     time.Time // time ping results were stored
+	ActivePaths    int // # of active paths (got echo reply)
+	ProbedPaths    int // # of probed paths (sent echo request)
+	AvailablePaths int // # of known paths
 }
 
 type DataExporter interface {
